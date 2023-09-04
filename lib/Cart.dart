c85b930d3188279f48f110bdda5ec84e3b3951be
Karthi981 <karkartgh@gmail.com>
2023-09-04 06:18:12 +0530

    with firebase 2.0

--- lib/Cart.dart
+++ lib/Cart.dart
@@ -37,6 +37,7 @@ class _CartState extends State<Cart> {
      setState(() {
        items=tempList;
      });
+     print(items[0]);
   }
 
 
@@ -91,26 +92,52 @@ class _CartState extends State<Cart> {
              return ListView.builder(
                  itemCount: items.length,
                  itemBuilder: (context,index) {
-                   return Padding(
-                     padding: const EdgeInsets.all(8.0),
-                     child: Container(
-                       height: 80,
+                   return
+                     Dismissible(
+                       // Each Dismissible must contain a Key. Keys allow Flutter to
+                       // uniquely identify widgets.
+                       key: Key(items[index]['title']),
+                       // Provide a function that tells the app
+                       // what to do after an item has been swiped away.
+                       onDismissed: (direction) {
+                         // Remove the item from the data source.
+                         setState(() {
+                           FirebaseFirestore.instance.collection('users').doc(uid).collection('cart').doc('1').delete();
+                           items.removeAt(index);
+
+                         });
+
+                         // Then show a snackbar.
+                         ScaffoldMessenger.of(context)
+                             .showSnackBar(SnackBar(content: Text('${items[index]['title']} dismissed')));
+                       },
+                       // Show a red background as the item is swiped away.
+                       background: Container(color: Colors.red),
                        child: ListTile(
                          title: Text(items[index]['title']),
-                         leading: Container(
-                           height: 50,
-                           width: 50,
-                           decoration: BoxDecoration(
-                             shape: BoxShape.rectangle,
-                             image: DecorationImage(image: NetworkImage(items[index]['image']))
-                           ),
-                         ),
-                         // leading: Text(items[index]['image'].toString()),
-                         trailing: Text("\$:${items[index]['price']}"),
-                         subtitle: Text(items[index]['rating']),
                        ),
-                     ),
-                   );
+                     );
+
+                   //   Padding(
+                   //   padding: const EdgeInsets.all(8.0),
+                   //   child: Container(
+                   //     height: 80,
+                   //     child: ListTile(
+                   //       title: Text(items[index]['title']),
+                   //       leading: Container(
+                   //         height: 50,
+                   //         width: 50,
+                   //         decoration: BoxDecoration(
+                   //           shape: BoxShape.rectangle,
+                   //           image: DecorationImage(image: NetworkImage(items[index]['image']))
+                   //         ),
+                   //       ),
+                   //       // leading: Text(items[index]['image'].toString()),
+                   //       trailing: Text("\$:${items[index]['price']}"),
+                   //       subtitle: Text(items[index]['rating']),
+                   //     ),
+                   //   ),
+                   // );
                  }
              );
            }
